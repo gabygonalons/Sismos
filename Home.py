76446f40eff8f51fc7bf39f25f23518ff881286b
Iván Miguel Cepeda <abog.ivancepeda@gmail.com>
2023-07-13 19:59:35 -0400

    cambio dashboard

--- Home.py
+++ Home.py
@@ -89,7 +89,7 @@ st.markdown("---")
 
 ######################################### DASSHBOARD ###################################################
 #CUERPO 2, "Dashboard análisis histórico", filtros, y gráficos
-st.markdown("<h1 style= 'text-align: center;'>Sísmos Mas Importantes</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style= 'text-align: center;'>Sismos Mas Importantes</h1>", unsafe_allow_html=True)
 mex = pd.read_csv(r"data mexico for analysis.csv")
 jp = pd.read_csv(r"data japan for analysis.csv")
 usa = pd.read_csv(r"data usa for analysis.csv")
@@ -111,8 +111,6 @@ top_loc_jp = jp['Location Name'].value_counts().head(10)
 data_usa = usa.filter(['Location Name', 'Latitude', 'Longitude'], axis=1)
 data_mex = mex.filter(['Location Name', 'Latitude', 'Longitude'], axis=1)
 data_jp = jp.filter(['Location Name', 'Latitude', 'Longitude'], axis=1)
-
-st.markdown("<h3 style= 'text-align: right;'>Dashboard</h3>", unsafe_allow_html=True)
 
 ############ FITROS ******
 st.sidebar.write("# Filtros para las visualizaciones")
@@ -140,7 +138,7 @@ else:
     sismos_por_anio = sismos_por_aniousa
 
 #year: start_year, end_year = st.slider('Seleccione un rango de año', options
-rango_anios = st.sidebar.select_slider('Selecciona un rango de años', options=list(range(1900, 2021)), value=(1900, 2020))
+rango_anios = st.sidebar.select_slider('Selecciona un rango de años', options=list(range(1900, 2023)), value=(1900, 2022))
 
 ################################## Gráficos ###################################
 with st.container():
@@ -148,16 +146,9 @@ with st.container():
     with col1:
     
         ##########Grafico 1 col2
-        fig = px.bar(y=top_loc.index, x=top_loc.values)
-        fig.update_traces(marker_color='orange', width=0.5)
-        fig.update_layout(xaxis_title='Localidades', yaxis_title='Cantidad de sismos', title='Cantidad de sismos por localidad', bargap=0.5)
-        st.plotly_chart(fig, use_container_width=True)
-
-        ##########Grafico 2 COL1
-
         data.rename(columns={'Latitude':'lat','Longitude':'lon'}, inplace=True)
         fig = px.scatter_mapbox(data, lat="lat", lon="lon", hover_name="Location Name", hover_data=["Location Name"],
-                                color_discrete_sequence=["orange"], zoom=zoom, height=400)
+                                color_discrete_sequence=["orange"], zoom=zoom, height=350)
         fig.update_layout(
             mapbox_style="white-bg",
             mapbox_layers=[
@@ -173,41 +164,43 @@ with st.container():
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
         st.plotly_chart(fig, use_container_width=True)
 
-    # Mapa
     with col2:
-        ###########GRAFICO 1 col 2.
-        # Crear el gráfico de dispersión solo para los datos de Estados Unidos
+        ##########Grafico 2 COL1
+        fig = px.bar(y=top_loc.index, x=top_loc.values)
+        fig.update_traces(marker_color='orange', width=0.5)
+        fig.update_layout(xaxis_title='Localidades', yaxis_title='Cantidad de sismos', title='Sismos por localidad', bargap=0.5, height=380)
+        st.plotly_chart(fig, use_container_width=True)
+        
+with st.container():        
+    col1, col2 = st.columns(2)        
     
-        # Filtrar los datos por el rango de años seleccionado
+    with col1:
+        ###########GRAFICO 1 col 2.
         indices_filtrados = [i for i, a in enumerate(anio) if a >= rango_anios[0] and a <= rango_anios[1]]
         x_filtrados = [anio[i] for i in indices_filtrados]
         y_filtrados = [mm[i] for i in indices_filtrados]
 
         # Crear el primer gráfico de dispersión
-        fig1 = go.Figure(data=go.Scatter(x=x_filtrados, y=y_filtrados, mode='markers', marker=dict(size=12, color='orange')))
-        fig1.update_layout(xaxis_title='Años', yaxis_title='Escala de Mercalli', title='Sismos por nivel de daño (Escala Mercalli)')
+        fig1 = go.Figure(data=go.Scatter(x=x_filtrados, y=y_filtrados, mode='markers', marker=dict(size=9, color='orange')))
+        fig1.update_layout(xaxis_title='Años', yaxis_title='Escala de Mercalli', yaxis=dict(range=[0, 12]), title='Sismos por nivel de daño (Escala Mercalli)', height=360)
         st.plotly_chart(fig1, use_container_width=True)
-
+    
+    with col2:
         ##################GRAFICO 2 Col 2
+        st.markdown(' ')
         # Filtrar los datos para el segundo gráfico
         sismos_filtrados = sismos_por_anio[(sismos_por_anio.index >= rango_anios[0]) & (sismos_por_anio.index <= rango_anios[1])]
 
         # Crear el segundo gráfico de líneas
         fig2 = px.line(x=sismos_filtrados.index, y=sismos_filtrados.values, markers=True)
         fig2.update_traces(line=dict(color='orange'))
-        fig2.update_layout(xaxis_title='Año', yaxis_title='Cantidad de sismos', title='Cantidad de sismos por año')
+        fig2.update_layout(xaxis_title='Año', yaxis_title='Cantidad de sismos', title='Cantidad de sismos por año', margin = dict(b=36), height=315)
         st.plotly_chart(fig2, use_container_width=True)
 
 st.markdown("---")
 
-import streamlit as st
- # Crear una variable de estado de sesión
-if 'page' not in st.session_state:
-    st.session_state.page = 1
- # Crear botones para navegar entre las páginas
 
-
-    
+     
 
 
 
--- pages/03_Sobre-Sistema-Notificación.py
+++ pages/03_Sobre-Sistema-Notificación.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go # para interactividad
+
 
 #Titulo1
 st.markdown("# Sobre el Sistemea de de Alertas Sísmicas")
@@ -26,11 +30,118 @@ st.markdown("Crear una base de datos, con la información sobre la actividad sí
 st.markdown("Identificar las zonas de mayor riesgo a través de la individualización de patrones y tendencias dentro de los datos.")
 st.markdown("Clasificar los eventos sísmicos a partir de sus características.")
 st.markdown("Informar sobre los posibles efectos secundarios de los eventos sísmicos conforme a su clasificación.")
-#KPIs
+##################################KPIs#########################################
 #Título h2
 st.markdown("## Indices Cláves de Rendimiento del Proyecto")
-#Parrafos
-st.markdown("Tasa de Click de la notificación a la app.")
+##################Transformaciones Tiempo de ejecución
+kpi = pd.read_csv('kpi_data.csv')
+# Obtiene los meses únicos en el DataFrame "kpi"
+meses_unicos = kpi['month'].unique()
+
+# Diccionario para almacenar los promedios por mes
+promedios_por_mes = {}
+
+# Calcula el promedio del tiempo de ejecución para cada mes y guarda los resultados
+for mes in meses_unicos:
+    promedio_mes = kpi[kpi['month'] == mes]['execution time'].mean()
+    promedios_por_mes[mes] = promedio_mes
+
+#############Transformación Tasa de satisfacción
+# Definir una función para transformar los valores a porcentajes
+def transform_to_percentage(value):
+    if value == 1:
+        return 0  # Completamente insatisfecho
+    elif value == 2:
+        return 20  # Insatisfecho
+    elif value == 3:
+        return 50  # Neutral
+    elif value == 4:
+        return 80  # Satisfecho
+    elif value == 5:
+        return 100  # Completamente satisfecho
+    else:
+        return None
+
+# Aplicar la transformación a la columna "user satisfaction"
+kpi['user satisfaction'] = kpi['user satisfaction'].apply(transform_to_percentage)
+
+# Agrupar los datos por mes y calcular la tasa de satisfacción del usuario
+satisfaction_by_month = kpi.groupby('month')['user satisfaction'].mean()
+
+st.markdown("### Tasa de Click de la notificación a la app.")
+# Convertir la columna 'date' a tipo fecha
+kpi['date'] = pd.to_datetime(kpi['date'])
+
+# Ordenar los datos por fecha
+kpi = kpi.sort_values('date')
+
+# Calcular la tasa de clic de las páginas con oferta premium
+kpi['click_rate_pages'] = (kpi['users on pages with premium offer'] / kpi['total users']) * 100
+
+# Calcular el incremento semanal en la tasa de clic
+kpi['weekly_increase'] = kpi['click_rate_pages'].pct_change(periods=7) + 1
+
+# Crear la figura del gráfico de la tasa de clic
+fig1 = go.Figure()
+
+fig1.add_trace(go.Indicator(
+    mode="number+gauge+delta",
+    value=kpi['click_rate_pages'].iloc[-1],  # Último valor de la tasa de clic
+    delta={'reference': 1.01, 'increasing': {'color': "green"}},
+    domain={'x': [0.1, 0.9], 'y': [0.2, 0.9]},
+    title={'text': "Tasa de Click de la notificación a la app"},
+    gauge={
+        'shape': 'angular',
+        'axis': {'range': [0, 10]},
+        'threshold': {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 5},
+        'bar': {'color': "orange"},
+        'steps': [
+            {'range': [0, 5], 'color': 'lightgray'},
+            {'range': [5, 10], 'color': 'gray'}
+        ]
+    }
+))
+
+fig1.update_layout(
+    autosize=False,
+    width=500,  # Aumentar el ancho del gráfico
+    height=400  # Aumentar la altura del gráfico
+)
+
+st.plotly_chart(fig1)
+
+
 st.markdown("Disminución de tasa de fallos.")
 st.markdown("Tiempo de ejecución de código.")
+fig = go.Figure(go.Indicator(
+    mode="number+gauge+delta",
+    gauge={'shape': "bullet"},
+    delta={'reference': promedios_por_mes[1], 'increasing': {'color': 'red'}, 'decreasing': {'color': 'green'}},
+    value=promedios_por_mes[2],
+    domain={'x': [0.1, 1], 'y': [0.2, 0.9]},
+    title={'text': "Tiempo<br>Promedio<br>de Ejecución", 'font': {'size': 13}},
+))
+
+# Personaliza el diseño del gráfico
+fig.update_layout(
+    height=250,  # Altura del gráfico
+    margin=dict(t=70),  # Espacio superior para el título
+)
+
+# Muestra el gráfico
+st.plotly_chart(fig, use_container_width=True)
 st.markdown("Tasa de satisfacción de la app.")
+## Satisfacción del mes 2 en relación al mes 1
+fig = go.Figure(go.Indicator(
+    mode = "number+delta",
+    value = satisfaction_by_month[2],
+    number = {'prefix': "%"},
+    delta = {'position': "top", 'reference': satisfaction_by_month[1]},
+    domain = {'x': [0, 1], 'y': [0, 1]}
+))
+
+fig.update_layout(
+    paper_bgcolor = "lightgray",
+    title = "User Satisfaction"  # Agregar la leyenda al gráfico
+)
+st.plotly_chart(fig, use_container_width=True)
