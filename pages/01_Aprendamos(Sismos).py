4010fd6781e335341968a266f6bb44f58b2789dc
Iván Miguel Cepeda <abog.ivancepeda@gmail.com>
2023-07-13 14:51:26 -0400

    pages

---
+++ pages/01_Aprendamos(Sismos).py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+#Titulo h1
+st.markdown("# Aprendamos")
+###################### BLOQUE 1# 
+#Título h2
+st.markdown("## ¿Qué son los sismos?")
+#parrafo 1
+st.markdown("Un terremoto​, también llamado sismo, temblor de tierra o movimiento telúrico, es la sacudida brusca y pasajera de la corteza terrestre. El punto de origen de un terremoto se denomina foco o hipocentro, a partir de allí se propaga en forma de ondas sísmicas.")
+#parrafo 2
+st.markdown("Los terremotos suelen estar asociados mayormente a una condición natural de la tierra como planeta, la cual tiene una capa superior llamada Litosfera, que se diferencia de las otras capas del planeta por ser sólida, más fría y encontrarse en la parte exterior. La litosfera está dividida en distintas áreas denominadas placas tectónicas.")
+#parrafo 3
+st.markdown("Los movimientos de las capas internas de la tierra, hacen que las placas tectónicas se muevan en distintas direcciones, se conoce que la mayoría de las manifestaciones sísmicas de la litosfera se producen en los límites de las placas, debido a la liberación de energía desde el punto de fricción, el sismo emite su fuerza en forma de 3 ondas de energía; llamadas primarias, secundarias y superficial, siendo la última la que mas nos afecta en forma de daños. Fuente: Instituto Geográfico Nacional de España, Sismología - Instituto Geográfico Nacional (ign.es)")
+#Título h3
+st.markdown("### Terremotos Inducidos")
+#parrafo
+st.markdown("Otra causa asociada a menor escala, son los terremotos inducidos, los cuales pueden estar relacionados con el llenado de embalses, agotamiento de los yacimientos de petróleo y gas, minería, eliminación subterránea de desechos, generación de energía geotérmica y franking. Fuente: libertaddigital.com, La verdad sobre la relación entre 'fracking' y terremotos - Libertad Digital")
+#Título h2
+st.markdown("## Magnitud y Profundidad")
+#Parrafo 1
+st.markdown("***La magnitud:*** de un sismo es una medida cuantitativa de la energía liberada durante el movimiento de las placas tectónicas. Se calcula utilizando escalas como la Escala de Richter o la Escala de Magnitud de Momento. Conocer la magnitud del sismo nos proporciona información sobre la fuerza y la violencia del terremoto en su origen. Es una medida fundamental para comparar y clasificar diferentes terremotos. Fuente: http://www.ssn.unam.mx/, Magnitud de un sismo")
+#Parafo 2
+st.markdown("***Profundidad del sismo:*** La profundidad del sismo se refiere a la ubicación vertical en la cual se origina el terremoto debajo de la superficie de la Tierra. Puede variar desde sismos superficiales hasta sismos profundos que se generan a gran profundidad. Conocer la profundidad del sismo es importante porque puede afectar la propagación de las ondas sísmicas y, por lo tanto, la forma en que el terremoto se siente en la superficie. Además, la profundidad puede influir en el nivel de daño causado por el sismo, ya que los terremotos más superficiales tienden a ser más destructivos en comparación con los sismos profundos. Fuente: SAGE, Efecto de la profundidad sobre la intensidad del temblor del suelo)")
+#Parrafo 3
+st.markdown("Por otro lado resulta relevante conocer la ***intensidad de los sismos***. Si bien la magnitud y la profundidad son medidas objetivas del terremoto, la intensidad es subjetiva puesto que se refiere a los efectos y daños que el sismo causa en áreas pobladas. Conocer la intensidad es crucial para comprender el impacto real del sismo en términos de daños a edificios, infraestructuras y la vida de las personas. También ayuda a guiar las labores de rescate y respuesta ante desastres. Se evalúa mediante la Escala de Mercalli o escalas similares, que describen los efectos observados en las personas, estructuras y el entorno.")
+#Parrafo 4
+st.markdown("En resumen, la magnitud y la profundidad del sismo proporcionan información sobre la fuerza y el origen del terremoto, mientras que la intensidad nos ayuda a comprender los efectos y el alcance de los daños causados. Estos datos son esenciales para la investigación sísmica, la planificación de la mitigación de riesgos, la respuesta a desastres y el diseño de estructuras resistentes a los terremotos.")
---
+++ pages/03_Sobre-Sistema-Notificación.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+#Titulo1
+st.markdown("# Sobre el Sistemea de de Alertas Sísmicas")
+#SupTítulo
+st.markdown("### Sobre el proyecto")
+#Parrafao
+st.markdown("La humanidad ha venido documentando cada uno de estos movimientos telúricos con el fin de crear mecanismos de educación, prevención y predicción. En la actualidad, GSN es una red en tiempo real cuyos datos son generados diariamente por grupos operativos de monitoreo, tanto en los Estados Unidos como a nivel internacional. En los Estados Unidos, el Centro Nacional de Información Sísmica recibe datos de todas las estaciones de GSN a nivel mundial en tiempo real para ubicar terremotos. Los datos de esta red son una entrada esencial para el sistema de alarma automatizado USGS PAGER (localizador) utilizado para evaluar de manera rápida y exacta la gravedad de los daños causados por un terremoto y para proporcionar información a organizaciones de socorro en emergencias, agencias gubernamentales y los medios de comunicación con una estimación del impacto social de la catástrofe potencial.")
+#Título 2
+st.markdown("## ¿Por qué realizar este sistema de alertas sísmicas?")
+#parrafo 2
+st.markdown("Los avances tecnológicos han permitido el accceso a los datos prácticamente en tiempo real desde cualquier lugar del planeta, no obstante la captación de datos se realiza desde diversas instituciones en distintos puntos del platena con equipamientos variados. Consecuentemente los datos resultantes de los monitoreos realizados presetan variados formatos y calidades por lo que resulta imperioso el pre-procesamiento de los mismos y su adecuación a los estandares de la Red.")
+#Titulo h2
+st.markdown("## Misión")
+#Parrafo
+st.markdown("Proveer a la alianza tri-alianza los datos requeridos en formatos acordes a los estándares de la red en plazos que permitan el cuplimiento cabal de los objetivos.")
+#Título h2
+st.markdown("## Visión")
+#Parrafo
+st.markdown("Colaborar con la alianza tri-nacional con el máximo nivel de eficacia, eficiencia y profesionalimo.")
+#Título h2
+st.markdown("## Objetivos")
+#Parrafo 
+st.markdown("Crear una base de datos, con la información sobre la actividad sísmica de los países miembros de la alianza: Estados Unidos, Japón y México.")
+
+st.markdown("Identificar las zonas de mayor riesgo a través de la individualización de patrones y tendencias dentro de los datos.")
+st.markdown("Clasificar los eventos sísmicos a partir de sus características.")
+st.markdown("Informar sobre los posibles efectos secundarios de los eventos sísmicos conforme a su clasificación.")
+#KPIs
+#Título h2
+st.markdown("## Indices Cláves de Rendimiento del Proyecto")
+#Parrafos
+st.markdown("Tasa de Click de la notificación a la app.")
+st.markdown("Disminución de tasa de fallos.")
+st.markdown("Tiempo de ejecución de código.")
+st.markdown("Tasa de satisfacción de la app.")
