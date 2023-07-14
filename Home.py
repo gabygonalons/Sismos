d11ddf992126f80d524af0da7109fa2b958b8d43
Iván Miguel Cepeda <abog.ivancepeda@gmail.com>
2023-07-13 23:20:00 -0400

    kpis

--- Home.py
+++ Home.py
@@ -5,6 +5,7 @@ from streamlit_folium import st_folium
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.io as pio
 
 
 ######################################### Cabecera #########################################
@@ -141,61 +142,235 @@ else:
 rango_anios = st.sidebar.select_slider('Selecciona un rango de años', options=list(range(1900, 2023)), value=(1900, 2022))
 
 ################################## Gráficos ###################################
-with st.container():
+
+tab1, tab2 = st.tabs(["Sísmos Importantes", "KPIs"])
+with tab1:
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+        
+            ##########Grafico 1 col2
+            data.rename(columns={'Latitude':'lat','Longitude':'lon'}, inplace=True)
+            fig = px.scatter_mapbox(data, lat="lat", lon="lon", hover_name="Location Name", hover_data=["Location Name"],
+                                    color_discrete_sequence=["orange"], zoom=zoom, height=350)
+            fig.update_layout(
+                mapbox_style="white-bg",
+                mapbox_layers=[
+                    {
+                        "below": 'traces',
+                        "sourcetype": "raster",
+                        "sourceattribution": "Cantidad de Sismos",
+                        "source": [
+                            "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+                        ]
+                    }
+                ])
+            fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+            st.plotly_chart(fig, use_container_width=True)
+
+        with col2:
+            ##########Grafico 2 COL1
+            fig = px.bar(y=top_loc.index, x=top_loc.values)
+            fig.update_traces(marker_color='orange', width=0.5)
+            fig.update_layout(xaxis_title='Localidades', yaxis_title='Cantidad de sismos', title='Sismos por localidad', bargap=0.5, height=380)
+            st.plotly_chart(fig, use_container_width=True)
+            
+    with st.container():        
+        col1, col2 = st.columns(2)        
+        
+        with col1:
+            ###########GRAFICO 1 col 2.
+            indices_filtrados = [i for i, a in enumerate(anio) if a >= rango_anios[0] and a <= rango_anios[1]]
+            x_filtrados = [anio[i] for i in indices_filtrados]
+            y_filtrados = [mm[i] for i in indices_filtrados]
+
+            # Crear el primer gráfico de dispersión
+            fig1 = go.Figure(data=go.Scatter(x=x_filtrados, y=y_filtrados, mode='markers', marker=dict(size=9, color='orange')))
+            fig1.update_layout(xaxis_title='Años', yaxis_title='Escala de Mercalli', yaxis=dict(range=[0, 12]), title='Sismos por nivel de daño (Escala Mercalli)', height=360)
+            st.plotly_chart(fig1, use_container_width=True)
+        
+        with col2:
+            ##################GRAFICO 2 Col 2
+            st.markdown(' ')
+            # Filtrar los datos para el segundo gráfico
+            sismos_filtrados = sismos_por_anio[(sismos_por_anio.index >= rango_anios[0]) & (sismos_por_anio.index <= rango_anios[1])]
+
+            # Crear el segundo gráfico de líneas
+            fig2 = px.line(x=sismos_filtrados.index, y=sismos_filtrados.values, markers=True)
+            fig2.update_traces(line=dict(color='orange'))
+            fig2.update_layout(xaxis_title='Año', yaxis_title='Cantidad de sismos', title='Cantidad de sismos por año', margin = dict(b=36), height=315)
+            st.plotly_chart(fig2, use_container_width=True)
+
+with tab2:
+    df = pd.read_csv('data_indicadores.csv')
     col1, col2 = st.columns(2)
     with col1:
-    
-        ##########Grafico 1 col2
-        data.rename(columns={'Latitude':'lat','Longitude':'lon'}, inplace=True)
-        fig = px.scatter_mapbox(data, lat="lat", lon="lon", hover_name="Location Name", hover_data=["Location Name"],
-                                color_discrete_sequence=["orange"], zoom=zoom, height=350)
+        # Convertir la columna 'date' a tipo fecha
+        df['date'] = pd.to_datetime(df['date'])
+
+        # Ordenar los datos por fecha
+        df = df.sort_values('date')
+
+        # Calcular la tasa de clic de las páginas con oferta premium
+        df['click_rate_pages'] = (df['users on pages with premium offer'] / df['total users']) * 100
+
+        # Crear la figura del gráfico de la tasa de clic
+        fig = go.Figure()
+
+        fig.add_trace(go.Indicator(
+            mode="number+gauge+delta",
+            value=df['click_rate_pages'].iloc[-1],  # Último valor de la tasa de clic
+            delta={'reference': 1.01, 'increasing': {'color': "green"}},
+            domain={'x': [0.1, 0.9], 'y': [0.2, 0.9]},
+            title={'text': "Tasa de Click de la notificación a la app"},
+            gauge={
+                'shape': 'angular',
+                'axis': {'range': [0, 100]},  # Actualizar el rango del eje vertical
+                'bar': {'color': "orange"},
+                'steps': [
+                    {'range': [0, 100], 'color': 'lightgray'},  # Eliminar los rangos de color
+                ]
+            }
+        ))
+
+        # Configurar la actualización de la tasa de clic en función del control deslizante
+        steps = []
+        for i, row in df.iterrows():
+            step = dict(
+                method='restyle',
+                args=['value', [row['click_rate_pages']]],
+                label=f"Week {row['month']}-{row['day']}"
+            )
+            steps.append(step)
+
+        sliders = [dict(
+            active=len(df)-1,
+            currentvalue={"prefix": "Week ", "visible": True, "xanchor": "center"},
+            pad={"t": 50},
+            steps=steps
+        )]
+
         fig.update_layout(
-            mapbox_style="white-bg",
-            mapbox_layers=[
-                {
-                    "below": 'traces',
-                    "sourcetype": "raster",
-                    "sourceattribution": "Cantidad de Sismos",
-                    "source": [
-                        "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-                    ]
-                }
-            ])
-        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+            sliders=sliders,
+            autosize=False,
+            width=500,  
+            height=400  
+        )
+
         st.plotly_chart(fig, use_container_width=True)
 
     with col2:
-        ##########Grafico 2 COL1
-        fig = px.bar(y=top_loc.index, x=top_loc.values)
-        fig.update_traces(marker_color='orange', width=0.5)
-        fig.update_layout(xaxis_title='Localidades', yaxis_title='Cantidad de sismos', title='Sismos por localidad', bargap=0.5, height=380)
+        # Convertir la columna 'date' a tipo fecha
+        df['date'] = pd.to_datetime(df['date'])
+
+        # Ordenar los datos por fecha
+        df = df.sort_values('date')
+
+        # Calcular la tasa de fallos
+        df['failure_rate'] = (df['failure_count'] / df['total users']) * 100
+
+        # Crear la figura del gráfico de la tasa de fallos
+        fig = go.Figure()
+
+        fig.add_trace(go.Indicator(
+            mode="number+gauge+delta",
+            value=df['failure_rate'].iloc[-1],  # Último valor de la tasa de fallos
+            delta={'reference': df['failure_rate'].iloc[-2], 'increasing': {'color': "red"}},
+            domain={'x': [0.1, 0.9], 'y': [0.2, 0.9]},
+            title={'text': "Tasa de fallos"},
+            gauge={
+                'shape': 'angular',
+                'axis': {'range': [0, max(df['failure_rate']) + 10]},  # Ajustar el rango del eje vertical
+                'bar': {'color': "orange"},
+                'steps': [
+                    {'range': [0, max(df['failure_rate'])], 'color': 'lightgray'},
+                ]
+            }
+        ))
+
+        # Configurar la actualización de la tasa de fallos en función del control deslizante
+        steps = []
+        for i, row in df.iterrows():
+            step = dict(
+                method='restyle',
+                args=['value', [row['failure_rate']]],
+                label=f"Week {row['month']}-{row['day']}"
+            )
+            steps.append(step)
+
+        sliders = [dict(
+            active=len(df)-1,
+            currentvalue={"prefix": "Week ", "visible": True, "xanchor": "center"},
+            pad={"t": 50},
+            steps=steps
+        )]
+
+        fig.update_layout(
+            sliders=sliders,
+            autosize=False,
+            width=500,
+            height=400
+        )
+
         st.plotly_chart(fig, use_container_width=True)
-        
-with st.container():        
-    col1, col2 = st.columns(2)        
-    
+
+    col1, col2 = st.columns(2)
     with col1:
-        ###########GRAFICO 1 col 2.
-        indices_filtrados = [i for i, a in enumerate(anio) if a >= rango_anios[0] and a <= rango_anios[1]]
-        x_filtrados = [anio[i] for i in indices_filtrados]
-        y_filtrados = [mm[i] for i in indices_filtrados]
-
-        # Crear el primer gráfico de dispersión
-        fig1 = go.Figure(data=go.Scatter(x=x_filtrados, y=y_filtrados, mode='markers', marker=dict(size=9, color='orange')))
-        fig1.update_layout(xaxis_title='Años', yaxis_title='Escala de Mercalli', yaxis=dict(range=[0, 12]), title='Sismos por nivel de daño (Escala Mercalli)', height=360)
-        st.plotly_chart(fig1, use_container_width=True)
-    
+        # Ordenar los datos por fecha
+        st.markdown("KPI FALTANTE")
     with col2:
-        ##################GRAFICO 2 Col 2
-        st.markdown(' ')
-        # Filtrar los datos para el segundo gráfico
-        sismos_filtrados = sismos_por_anio[(sismos_por_anio.index >= rango_anios[0]) & (sismos_por_anio.index <= rango_anios[1])]
+        df = df.sort_values('date')
 
-        # Crear el segundo gráfico de líneas
-        fig2 = px.line(x=sismos_filtrados.index, y=sismos_filtrados.values, markers=True)
-        fig2.update_traces(line=dict(color='orange'))
-        fig2.update_layout(xaxis_title='Año', yaxis_title='Cantidad de sismos', title='Cantidad de sismos por año', margin = dict(b=36), height=315)
-        st.plotly_chart(fig2, use_container_width=True)
+        # Calcular la tasa de satisfacción promedio por mes y asignarla a la columna "satisfaction_month"
+        df['satisfaction_month'] = df.groupby('month')['user satisfaction'].transform('mean')
+
+        # Crear la figura del gráfico de la tasa de satisfacción
+        fig = go.Figure()
+
+        fig.add_trace(go.Indicator(
+            mode="number+gauge+delta",
+            value=df['satisfaction_month'].iloc[-1],  # Último valor de la tasa de satisfacción
+            delta={'reference': df['satisfaction_month'].iloc[-2], 'increasing': {'color': "red"}},
+            domain={'x': [0.1, 0.9], 'y': [0.2, 0.9]},
+            title={'text': "Tasa de satisfacción"},
+            gauge={
+                'shape': 'angular',
+                'axis': {'range': [0, max(df['satisfaction_month']) + 10]},  # Ajustar el rango del eje vertical
+                'bar': {'color': "orange"},
+                'steps': [
+                    {'range': [0, max(df['satisfaction_month'])], 'color': 'lightgray'},
+                ]
+            }
+        ))
+
+        # Configurar la actualización de la tasa de satisfacción en función del control deslizante
+        steps = []
+        for i, row in df.iterrows():
+            step = dict(
+                method='restyle',
+                args=['value', [row['satisfaction_month']]],
+                label=f"Month {row['month']}"
+            )
+            steps.append(step)
+
+        sliders = [dict(
+            active=len(df)-1,
+            currentvalue={"prefix": "Month ", "visible": True, "xanchor": "center"},
+            pad={"t": 50},
+            steps=steps
+        )]
+
+        fig.update_layout(
+            sliders=sliders,
+            autosize=False,
+            width=500,
+            height=400
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
+
 
 st.markdown("---")
 
