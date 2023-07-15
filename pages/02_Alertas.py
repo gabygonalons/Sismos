d4a1c0ab4861f022b02ec13da11f4c664883debd
Gabriela Beatriz Goñalons <119463275+gabygonalons@users.noreply.github.com>
2023-07-15 17:42:12 -0300

    Update 02_Alertas.py

--- pages/02_Alertas.py
+++ pages/02_Alertas.py
@@ -3,14 +3,19 @@ import streamlit as st
 #Título H1
 st.markdown("# Alertas")
 #título h2
-st.markdown("## ¿Qué son las Alertas sísmicas?")
+st.markdown("## ¿Qué son las Alertas Sísmicas?")
 #parrafo 1
 st.markdown("""Las alertas sísmicas son señales de prevención ante sismos fuertes que se detectan en zonas sísmicas. La señal se envía por radio a las ciudades que tienen cobertura del Sistema de Alerta Sísmica Mexicano (SASMEX). La señal puede ser pública o preventiva según la estimación de los efectos del sismo. La señal llega con un tiempo variable de anticipación, que puede ser de 50 segundos o más, dependiendo de la distancia entre el epicentro y la ciudad. La alerta sísmica tiene el objetivo de contribuir a mitigar los daños y facilitar las acciones de seguridad de la población1.
 Las alarmas sísmicas modernas están compuestas por diversos componentes, tales como acelerómetros, sismómetros, procesadores de datos, entre otros. En todos los casos, el aparato se activará cuando ocurra un movimiento telúrico2""")
 #Título h3
-st.markdown("### Tipos de alertas")
+st.markdown("### Tipos de Alertas")
 #parrafo 2
 st.markdown("")
-#Títulos h2
+#Título h2
 st.markdown("## ¿Que hacer ante una alerta?")
 #parrafo 3
+st.markdown("Al sonar la alerta sísmica, mantén la calma y si te encuentras en pisos bajos y te es posible, evacúa el inmueble y sigue las rutas de evacuación previamente establecidas. En caso de encontrarte en un piso alto y no tienes tiempo de salir, colócate en la zona de menor riesgo previamente establecida.")
+#Título h2
+st.markdown("## Medidas Preventivas para mitigar las consecuencias")
+#parrafo 4
+st.markdown(
