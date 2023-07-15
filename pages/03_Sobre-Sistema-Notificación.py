24c06329f75f890a474c77d88b08a84d93462d63
Gabriela Beatriz Goñalons <119463275+gabygonalons@users.noreply.github.com>
2023-07-15 16:50:37 -0300

    Update 03_Sobre-Sistema-Notificación.py

--- pages/03_Sobre-Sistema-Notificación.py
+++ pages/03_Sobre-Sistema-Notificación.py
@@ -5,13 +5,13 @@ import plotly.graph_objects as go # para interactividad
 
 
 #Titulo1
-st.markdown("# Sobre el Sistemea de de Alertas Sísmicas")
+st.markdown("# Sobre el Sistemea de Notificaciones Sísmicas")
 #SupTítulo
 st.markdown("### Sobre el proyecto")
 #Parrafao
-st.markdown("La humanidad ha venido documentando cada uno de estos movimientos telúricos con el fin de crear mecanismos de educación, prevención y predicción. En la actualidad, GSN es una red en tiempo real cuyos datos son generados diariamente por grupos operativos de monitoreo, tanto en los Estados Unidos como a nivel internacional. En los Estados Unidos, el Centro Nacional de Información Sísmica recibe datos de todas las estaciones de GSN a nivel mundial en tiempo real para ubicar terremotos. Los datos de esta red son una entrada esencial para el sistema de alarma automatizado USGS PAGER (localizador) utilizado para evaluar de manera rápida y exacta la gravedad de los daños causados por un terremoto y para proporcionar información a organizaciones de socorro en emergencias, agencias gubernamentales y los medios de comunicación con una estimación del impacto social de la catástrofe potencial.")
+st.markdown("La humanidad ha venido documentando cada uno de estos movimientos telúricos con el fin de crear mecanismos para la educación, prevención y predicción. En la actualidad, GSN es una red en tiempo real cuyos datos son generados diariamente por grupos operativos de monitoreo, tanto en los Estados Unidos como a nivel internacional. En los Estados Unidos, el Centro Nacional de Información Sísmica recibe datos de todas las estaciones de GSN a nivel mundial en tiempo real para ubicar terremotos. Los datos de esta red son una entrada esencial para el sistema de alarma automatizado USGS PAGER (localizador) utilizado para evaluar de manera rápida y exacta la gravedad de los daños causados por un terremoto y para proporcionar información a organizaciones de socorro en emergencias, agencias gubernamentales y los medios de comunicación con una estimación del impacto social de la catástrofe potencial.")
 #Título 2
-st.markdown("## ¿Por qué realizar este sistema de alertas sísmicas?")
+st.markdown("## ¿Por qué realizar este sistema de notificaciones sísmicas?")
 #parrafo 2
 st.markdown("Los avances tecnológicos han permitido el accceso a los datos prácticamente en tiempo real desde cualquier lugar del planeta, no obstante la captación de datos se realiza desde diversas instituciones en distintos puntos del platena con equipamientos variados. Consecuentemente los datos resultantes de los monitoreos realizados presetan variados formatos y calidades por lo que resulta imperioso el pre-procesamiento de los mismos y su adecuación a los estandares de la Red.")
 #Titulo h2
@@ -25,16 +25,16 @@ st.markdown("Colaborar con la alianza tri-nacional con el máximo nivel de efica
 #Título h2
 st.markdown("## Objetivos")
 #Parrafo 
-st.markdown("Crear una base de datos, con la información sobre la actividad sísmica de los países miembros de la alianza: Estados Unidos, Japón y México.")
+st.markdown("Crear una base de datos, con información sobre la actividad sísmica de los países miembros de la alianza: Estados Unidos, Japón y México.")
 
 st.markdown("Identificar las zonas de mayor riesgo a través de la individualización de patrones y tendencias dentro de los datos.")
 st.markdown("Clasificar los eventos sísmicos a partir de sus características.")
 st.markdown("Informar sobre los posibles efectos secundarios de los eventos sísmicos conforme a su clasificación.")
 ##################################KPIs#########################################
 #Título h2
-st.markdown("## Indices Cláves de Rendimiento del Proyecto")
+st.markdown("## Indices Claves de Rendimiento del Proyecto")
 ##################Transformaciones Tiempo de ejecución
-st.markdown("Tasa de Click de la notificación a la app.")
-st.markdown("Disminución de tasa de fallos.")
-st.markdown("Tiempo de ejecución de código.")
-st.markdown("Tasa de satisfacción de la app.")
+st.markdown("Tasa de Click de la notificación a la app")
+st.markdown("Disminución de tasa de fallos")
+st.markdown("Tiempo de ejecución de código")
+st.markdown("Tasa de satisfacción de la app")
