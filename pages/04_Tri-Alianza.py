45ef9ffd210b5af1b7fc952136f99aea09a205a3
Gabriela Beatriz Goñalons <119463275+gabygonalons@users.noreply.github.com>
2023-07-17 13:13:39 -0300

    Update 04_Tri-Alianza.py
    Actulización de info, Tri-alianza

--- pages/04_Tri-Alianza.py
+++ pages/04_Tri-Alianza.py
@@ -1,13 +1,19 @@
 import streamlit as st
 
 #Título h1
-st.markdown("# Tri-Alianza")
+st.markdown("# **“Working towards global standardization of seismological networks and effective communication to the civilian community”**")
 #Título h2
-st.markdown("## ¿Qué es la Tri-Alianza?")
-#Parrafo 1
-st.markdown("")
-#Título h3
-st.markdown("### Estado de la situación actual")
-#Parrafo 2
-st.markdown("")
+st.markdown("## ¿Qué es la Tri-Alianza?)
+#Párrafo 1
+st.markdown("## La tri-alianza es un acuerdo entre tres países para aunar esfuerzos en post del sostenimiento y la mejora contínua de redes de información sobre eventos sísmicos.")
+#Título h2
+st.markdown("## ¿Qué países integran la tri-alianza?")
+Párrafo 2
+st.markdown("## La alianza está integrada por el *Servicio Geológico de Estados Unidos* **USGS**, la *Agencia Meteorológica de Japón* **JMA** y el *Servicio Sismológico Nacional de México* **SSN**.")
+#Título h2
+st.markdown("## ¿Cuáles son los **objetivos** de la tri-alianza?")
+Párrafo 3
+st.markdown("## La trialianza tiene como principales **objetivos**:")
+st.markdown("## - Trabajar por la estandarización los datos que alimentan las redes de información sísmica a nivel global")
+st.markdown("## - La comunicación efectiva a la comunidad civil de los eventos sísmicos y sus consecuencias.")
 
