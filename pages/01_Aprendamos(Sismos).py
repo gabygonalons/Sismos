aa2a1da344336351139ef9cde72bbaa2df5bab25
Gabriela Beatriz Goñalons <119463275+gabygonalons@users.noreply.github.com>
2023-07-17 16:37:35 -0300

    Update 01_Aprendamos(Sismos).py

--- pages/01_Aprendamos(Sismos).py
+++ pages/01_Aprendamos(Sismos).py
@@ -5,22 +5,28 @@ st.markdown("# Aprendamos")
 ###################### BLOQUE 1# 
 #Título h2
 st.markdown("## ¿Qué son los Sismos?")
-#parrafo 1
+#Párrafo 1
 st.markdown("Un terremoto​, también llamado sismo, temblor de tierra o movimiento telúrico, es la sacudida brusca y pasajera de la corteza terrestre. El punto de origen de un terremoto se denomina foco o hipocentro, a partir de allí se propaga en forma de ondas sísmicas.")
-#parrafo 2
-st.markdown("Los terremotos suelen estar asociados mayormente a una condición natural de la tierra como planeta, la cual tiene una capa superior llamada Litósfera, que se diferencia de las otras capas del planeta por ser sólida, más fría y encontrarse en la parte exterior. La Litósfera está dividida en distintas capas denominadas placas tectónicas.")
-#parrafo 3
-st.markdown("Los movimientos de las capas internas de la tierra, hacen que las placas tectónicas se muevan en distintas direcciones, se conoce que la mayoría de las manifestaciones sísmicas de la litósfera se producen en los límites de las placas, debido a la liberación de energía desde el punto de fricción, el sismo emite su fuerza en forma de 3 ondas de energía; llamadas primarias, secundarias y superficial, siendo la última la que mas nos afecta en forma de daños. Fuente: Instituto Geográfico Nacional de España, Sismología - Instituto Geográfico Nacional (ign.es)")
-#Título h3
-st.markdown("### Terremotos Inducidos")
-#parrafo
-st.markdown("Otra causa asociada a menor escala, son los terremotos inducidos, los cuales pueden estar relacionados con el llenado de embalses, agotamiento de los yacimientos de petróleo y gas, minería, eliminación subterránea de desechos, generación de energía geotérmica y franking. Fuente: libertaddigital.com, La verdad sobre la relación entre 'fracking' y terremotos - Libertad Digital")
+#Párrafo 2
+st.markdown("Los terremotos suelen estar asociados mayormente a una condición natural de la tierra como planeta, la cual tiene una capa superior llamada litósfera, que se diferencia de las otras capas del planeta por ser sólida, más fría y encontrarse en la parte exterior. La Litósfera está dividida en distintas capas denominadas placas tectónicas.")
+#Párrafo 3
+st.markdown("Los movimientos de las capas internas de la tierra, hacen que las placas tectónicas se muevan en distintas direcciones, se conoce que la mayoría de las manifestaciones sísmicas de la litósfera se producen en los límites de las placas, debido a la liberación de energía desde el punto de fricción, el sismo emite su fuerza en forma de 3 ondas de energía; llamadas primarias, secundarias y superficial, siendo la última la que mas nos afecta en forma de daños. Fuente: Instituto Geográfico Nacional de España.") [Sismología - Instituto Geográfico Nacional](https://www.ign.es/web/sis-area-sismicidad")
+#Párrafo 4
+st.markdown("Otra causa asociada a menor escala, son los terremotos inducidos, los cuales pueden estar relacionados con el llenado de embalses, agotamiento de los yacimientos de petróleo y gas, minería, eliminación subterránea de desechos, generación de energía geotérmica y franking. Fuente: La verdad sobre la relación entre 'fracking' y terremotos.") [Libertad Digital](https://www.libertaddigital.com/ciencia-tecnologia/ciencia/2015-02-25/la-verdad-sobre-la-relacion-entre-fracking-y-terremotos-1276541587/)
 #Título h2
-st.markdown("## Magnitud y Profundidad")
-#Parrafo 1
+st.markdown("## ¿Cómo se miden los sismos?")
+#Párrafo 1
+st.marckdown("## Los movimientos sísmicos son detectados con unos aparatos llamados sismógrafos y acelerógrafos, que ayudan a medir el tamaño del movimiento en diversas direcciones. Al ocurrir un sismo, se puede medir tanto su magnitud como su intensidad. Para ello, se utilizan varias escalas, pero las más comunes son la **escala de Richter**, que brinda información sobre la *magnitud o "causa" del siniestro*; y la de **Mercalli**, que indica la *intensidad o "efectos"* del mismo.")
+#Párrafo2
+st.marckdown("## A través de la escala de Richter, nombrada así en honor a un sismólogo estadounidense, se puede conocer el **"tamaño"** del sismo. Dicha cifra, calculada mediante una expresión matemática, dará cuenta de la cantidad de energía liberada en la zona interior de la tierra donde se inició la fractura que dio origen al sismo.")
+#Párrafo 3
+st.marckdown("## La escala de Mercalli, en cambio, dará información basada en los **efectos** que produjo el sismo sobre las personas, objetos y espacios. A diferencia de la magnitud, este dato variará según la distancia de cada lugar hacia el foco del sismo, con lo que un sismo tendrá una magnitud única pero intensidades diferentes en cada localidad.")
+#Título h2
+st.markdown("## ¿Qué expresan la magnitud y la profundidad de un sismo?")
+#Párrafo 1
 st.markdown("***La magnitud:*** de un sismo es una medida cuantitativa de la energía liberada durante el movimiento de las placas tectónicas. Se calcula utilizando escalas como la Escala de Richter o la Escala de Magnitud de Momento. Conocer la magnitud del sismo nos proporciona información sobre la fuerza y la violencia del terremoto en su origen. Es una medida fundamental para comparar y clasificar diferentes terremotos. Fuente: http://www.ssn.unam.mx/, Magnitud de un sismo")
 #Parafo 2
-st.markdown("***Profundidad del sismo:*** La profundidad del sismo se refiere a la ubicación vertical en la cual se origina el terremoto debajo de la superficie de la Tierra. Puede variar desde sismos superficiales hasta sismos profundos que se generan a gran profundidad. Conocer la profundidad del sismo es importante porque puede afectar la propagación de las ondas sísmicas y por lo tanto, la forma en que el terremoto se siente en la superficie. Además, la profundidad puede influir en el nivel de daño causado por el sismo, ya que los terremotos más superficiales tienden a ser más destructivos en comparación con los sismos profundos. Fuente: SAGE, Efecto de la profundidad sobre la intensidad del temblor del suelo)")
+st.markdown("***Profundidad del sismo:*** la profundidad del sismo se refiere a la ubicación vertical en la cual se origina el terremoto debajo de la superficie de la Tierra. Puede variar desde sismos superficiales hasta sismos profundos que se generan a gran profundidad. Conocer la profundidad del sismo es importante porque puede afectar la propagación de las ondas sísmicas y por lo tanto, la forma en que el terremoto se siente en la superficie. Además, la profundidad puede influir en el nivel de daño causado por el sismo, ya que los terremotos más superficiales tienden a ser más destructivos en comparación con los sismos profundos. Fuente: SAGE, Efecto de la profundidad sobre la intensidad del temblor del suelo)")
 #Parrafo 3
 st.markdown("Por otro lado resulta relevante conocer la ***intensidad de los sismos***. Si bien la magnitud y la profundidad son medidas objetivas del terremoto, la intensidad es subjetiva puesto que se refiere a los efectos y daños que el sismo causa en áreas pobladas. Conocer la intensidad es crucial para comprender el impacto real del sismo en términos de daños a edificios, infraestructuras y la vida de las personas. También ayuda a guiar las labores de rescate y respuesta ante desastres. Se evalúa mediante la Escala de Mercalli o escalas similares, que describen los efectos observados en las personas, estructuras y el entorno.")
 #Parrafo 4
