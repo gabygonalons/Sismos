1853e51daa1ec1e581f20a5974e93ada5c870ce4
Iván Miguel Cepeda <abog.ivancepeda@gmail.com>
2023-07-17 20:43:40 -0400

    revisión

--- pages/02_Alertas.py
+++ pages/02_Alertas.py
@@ -5,18 +5,18 @@ st.markdown("# Alertas")
 #Título h2
 st.markdown("## ¿Se pueden predecir los sismos?")
 #Párrafo 1
-st.markdown("## Hasta hoy, no existe una técnica que permita predecir los sismos. Ni países como Estados Unidos y Japón cuya tecnología es muy avanzada, han sido capaces de desarrollar una técnica predictiva de temblores. La única certeza que existe es quela tierra tiembla constantemente y que debemos estar preparados. Ante cualquier evento sísmico lo único que nos puede ayudar es la prevención.")
+st.markdown(" Hasta hoy, no existe una técnica que permita predecir los sismos. Ni países como Estados Unidos y Japón cuya tecnología es muy avanzada, han sido capaces de desarrollar una técnica predictiva de temblores. La única certeza que existe es quela tierra tiembla constantemente y que debemos estar preparados. Ante cualquier evento sísmico lo único que nos puede ayudar es la prevención.")
 #Título h2
 st.markdown("## ¿Qué son las Alertas sísmicas?")
 #Párrafo 2
-st.markdown("## Las alertas sísmicas, son señales de advertensia que permiten iniciar oportunamente procedimientos y acciones de prevención segundos antes del arribo de las ondas sísmicas que pueden ocasionar daños.")
+st.markdown(" Las alertas sísmicas, son señales de advertensia que permiten iniciar oportunamente procedimientos y acciones de prevención segundos antes del arribo de las ondas sísmicas que pueden ocasionar daños.")
 #Título h2
 st.markdown("## Tipos de alertas")
 #Párrafo 3
-st.markdown("## Las alertas sísmicas funcionan de distintas formas dependiendo del país en que se encuentran, algunas con tecnología más avanzada y otras más efectivas al igual que menos ruidosas para evitar aún más el miedo.")
-st.markdown("# - Uno de los países que tiene más actualizado su sistema de alertas es **Japón**, que como se sabe, es frecuente la ocurrencia de algún nuevo sismo. Debido a anteriores movomientos terrestres que han superado los 7.0 de magnitud han avanzado su tecnología para alertar a la población.")
-st.markdown("# - La región de **Estados Unidos** más propensa a terremotos es la costa oeste, por lo que tiene un robusto sistema de alerta sísmica. El objetivo de las alertas sísmicas es que las personas tomen las medidas necesarias para salvar sus vidas, ya que puede advertir a la población hasta con 10 segundos de anticipación. Earthquake Warning California puede dar alertas a través de las siguientes herramientas: la aplicación MyShake, alertas de Android y alertas Inalámbricas de Emergencia (WEA).")
-st.markdown("# - En **Mexico** un sistema de sensores sísmicos distribuidos en el centro y la costa oeste de México está diseñado para detectar movimientos sísmicos y emitir alertas tempranas. El SASMEX emite 2 tipos de alerta: Alerta Pública y Alerta Preventiva. Si los efectos del sismo se estiman en efectos *Moderados*, se emitirá una *Alerta Preventiva*; si se estiman efectos *fuertes*, se emitirá una Alerta Pública.")
+st.markdown(" Las alertas sísmicas funcionan de distintas formas dependiendo del país en que se encuentran, algunas con tecnología más avanzada y otras más efectivas al igual que menos ruidosas para evitar aún más el miedo.")
+st.markdown(" - Uno de los países que tiene más actualizado su sistema de alertas es **Japón**, que como se sabe, es frecuente la ocurrencia de algún nuevo sismo. Debido a anteriores movomientos terrestres que han superado los 7.0 de magnitud han avanzado su tecnología para alertar a la población.")
+st.markdown(" - La región de **Estados Unidos** más propensa a terremotos es la costa oeste, por lo que tiene un robusto sistema de alerta sísmica. El objetivo de las alertas sísmicas es que las personas tomen las medidas necesarias para salvar sus vidas, ya que puede advertir a la población hasta con 10 segundos de anticipación. Earthquake Warning California puede dar alertas a través de las siguientes herramientas: la aplicación MyShake, alertas de Android y alertas Inalámbricas de Emergencia (WEA).")
+st.markdown(" - En **Mexico** un sistema de sensores sísmicos distribuidos en el centro y la costa oeste de México está diseñado para detectar movimientos sísmicos y emitir alertas tempranas. El SASMEX emite 2 tipos de alerta: Alerta Pública y Alerta Preventiva. Si los efectos del sismo se estiman en efectos *Moderados*, se emitirá una *Alerta Preventiva*; si se estiman efectos *fuertes*, se emitirá una Alerta Pública.")
 #Títulos h2
 st.markdown("## ¿Que si estamos en una zona sísmica?")
 #Párrafo 3 ("## Cada país brinda información y prepara a la población, a continuación un ejemplo de *Campaña Preventiva* impulsada por el Gobierno de México.")
